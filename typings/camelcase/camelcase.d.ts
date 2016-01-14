ff75c69a1e16ad3e6da3b6ccd142dbae1618ef74
Sam Verschueren <sam.verschueren@gmail.com>
2016-01-14 13:01:59 +0100

    remove typings

--- typings/camelcase/camelcase.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for camelcase
-// Project: https://github.com/sindresorhus/camelcase
-// Definitions by: Sam Verschueren <https://github.com/samverschueren>
-
-declare module "camelcase" {
-	function camelcase(...args: string[]): string;
-	export = camelcase;
-}
