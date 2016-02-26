e04912e00ad901f0124244aac57e8a53349f3082
Sam Verschueren <sam.verschueren@gmail.com>
2016-02-26 13:21:41 +0100

    add basic routing functionality - resolves #6

--- router.ts
+++ router.ts
@@ -1,3 +1,4 @@
+import {Inject} from './core';
 import * as dotProp from 'dot-prop';
 
 export interface RouteDefinition {
@@ -29,3 +30,59 @@ export function RouteConfig(routes: RouteDefinition[]) {
 		annotate(target, 'routes', routes);
 	};
 }
+
+/**
+ * Providers
+ */
+
+export class Instruction {
+	_state: string;
+	urlPath: string;
+	urlParams: string;
+}
+
+export class Router {
+
+	constructor(@Inject('$state') private state) {
+
+	}
+
+	isRouteActive(instruction: Instruction): boolean {
+		return this.state.is(instruction._state, instruction.urlParams);
+	}
+
+	navigate(linkParams: any[]): Promise<any> {
+		return this.state.go(linkParams[0], linkParams[1] || {});
+	}
+
+	renavigate(): Promise<any> {
+		return this.state.reload(this.state.current);
+	}
+
+	generate(linkParams: any[]): Promise<Instruction> {
+		const state = linkParams[0];
+		const params = linkParams[1] || {};
+		const url = this.state.href(state, params);
+
+		const instruction = new Instruction();
+		instruction._state = state;
+		instruction.urlPath = this.state.href(state, params);
+		instruction.urlParams = params;
+
+		return Promise.resolve(instruction);
+	}
+}
+
+export class RouteParams {
+
+	constructor(@Inject('$stateParams') private stateParams) {
+
+	}
+
+	get(param: string): string {
+		return this.stateParams[param];
+	}
+}
+
+// Export the providers
+export const ROUTER_PROVIDERS = [Router, RouteParams];
