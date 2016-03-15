8034b4d02ab1d4445d039d4eeb7b7b2159a02c5c
Sam Verschueren <sam.verschueren@gmail.com>
2016-03-15 11:10:52 +0100

    inject services in the pipe constructor - fixes #31

--- src/platform/bootstrap/pipe.ts
+++ src/platform/bootstrap/pipe.ts
@@ -1,17 +1,25 @@
+import * as utils from './utils';
+
 export function bootstrap(ngModule, target) {
 	const pipe = target.__annotations__.pipe;
+	utils.inject(target);
 
-	ngModule
-		.filter(pipe.name, () => {
-			return function(value, ...args: any[]) {
-				if (pipe.pure === false) {
-					const instance = new target();
-					return instance.transform(value, args);
-				}
+	const filter = target.$inject || [];
+	filter.push((...args) => {
+		// Create a new instance and inject the dependencies
+		const instance = new target(...args);
 
-				return target.prototype.transform(value, args);
-			}
-		});
+		// Create the filter function
+		const filter: any = function(value, ...args: any[]) {
+			return instance.transform(value, args);
+		}
 
+		// If pure is set to false, it's a stateful filter
+		filter.$stateful = pipe.pure === false;
+
+		return filter;
+	});
+
+	ngModule.filter(pipe.name, filter);
 	return pipe.name;
 }
