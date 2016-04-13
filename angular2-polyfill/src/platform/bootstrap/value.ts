69f1c853ce074922c769b9191245566a0ee3c29c
Sam Verschueren <sam.verschueren@gmail.com>
2016-04-13 11:57:58 +0200

    remove console.log

--- angular2-polyfill/src/platform/bootstrap/value.ts
+++ angular2-polyfill/src/platform/bootstrap/value.ts
@@ -4,9 +4,6 @@ import * as utils from './utils';
 export function bootstrap(ngModule, target) {
 	const value = target.__annotations__.value;
 	const name = value.name;
-
-	console.log(value);
-
 	ngModule.value(name, value.value);
 	return name;
 }
